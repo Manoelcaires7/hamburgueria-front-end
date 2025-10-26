5b7a7a4297123aa7d5e87f7bee3a03081e7568c3
Manoelcaires7 <manelcairess0707@gmail.com>
2025-10-26 19:02:32 -0300

    Estilização e adição da Imagem para o Banner do cardapio

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,24 +1,26 @@
 import { Container, Banner, CategoryMenu, ProductsContainer} from "./styles"
 
 export function Menu(){
-    <Container>
-        <Banner>
-            <h1>O MELHOR
-                <br />
-                HAMBURGUER
-                <br />
-                ESTÁ AQUI!
-            </h1>
+    return (
+        <Container>
+            <Banner>
+                <h1>O MELHOR
+                    <br />
+                    HAMBURGUER
+                    <br />
+                    ESTÁ AQUI!
+                    <span>Esse cardápio está irresistível!</span>
+                </h1>
 
-            <span>Esse cardápio está irresistível!</span>
-        </Banner>
-        <CategoryMenu>
+            </Banner>
+            <CategoryMenu>
 
-        </CategoryMenu>
+            </CategoryMenu>
 
-        <ProductsContainer>
+            <ProductsContainer>
 
-        </ProductsContainer>
+            </ProductsContainer>
 
-    </Container>
+        </Container>
+    );
 }
--- src/containers/Menu/styles.js
+++ src/containers/Menu/styles.js
@@ -1,8 +1,44 @@
 import styled from "styled-components";
+import BannerHamburguer from '../../assets/tela-cardapio.svg'
 
-export const Container = styled.div``
+export const Container = styled.div`
+width: 100%;
+min-height: 100vh;
+background-color: #f0f0f0;
+`
 
-export const Banner = styled.div``
+export const Banner = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 480px;
+width: 100%;
+position: relative;
+
+background: url('${BannerHamburguer}') no-repeat;
+background-color: #1f1f1f;
+background-position: center;
+background-size: cover;
+
+
+h1{
+    font-family: 'Road Rage', sans-serif;
+    font-size: 80px;
+    line-height: 65px;
+    position: absolute;
+    color: #fff;
+
+    right: 20%;
+    top: 30%;
+
+    span{
+        display: block;
+        color: #fff;
+        font-size: 20px;
+    }
+}
+
+`
 
 export const CategoryMenu = styled.div``
 
