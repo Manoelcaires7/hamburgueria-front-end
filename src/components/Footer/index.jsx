7bf5c4b82eebe5beff9f8fcc4d9fba4ce684fa68
Manoelcaires7 <manelcairess0707@gmail.com>
2025-11-10 15:59:15 -0300

    feat: Adiciona componente Footer com estilos e informações de direitos autorais

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export function Footer(){
+    return(
+        <Container>
+            <p>
+            Desenvolvido por Manoel - 2025 - Todos os Direitos Reservados
+            </p>
+        </Container>
+    )
+}
